3a4a03b3041f9b4f3c1e49bb89b924cd91161712
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-11-30 18:22:51 +0200

    Create search request

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
+token = os.environ["TOKEN"]
 params = {
         "q": "Кока-Кола",
-        "access_token": "aab36c77aab36c77aab36c770aa9a51addaaab3aab36c77cfd55b3447a5a9eab522c07e",
+        "access_token": token,
         "v": "5.199"
     }
 response = requests.get("https://api.vk.com/method/newsfeed.search", params=params)
 response.raise_for_status()
 result = response.json()
-print(result)
+print(result.keys)
