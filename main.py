6b832938d47364eb33784f003bded9ab3c577fd6
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-11-30 19:56:49 +0200

    Create search by date

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import requests
 import os
 from dotenv import load_dotenv
@@ -6,11 +8,20 @@ load_dotenv()
 
 token = os.environ["TOKEN"]
 params = {
-        "q": "Кока-Кола",
+        "q": "Coca-Cola",
         "access_token": token,
+        "start_time": 1701295140,
+        "end_time": 1701381540,
         "v": "5.199"
     }
 response = requests.get("https://api.vk.com/method/newsfeed.search", params=params)
 response.raise_for_status()
 result = response.json()
-print(result.keys)
+values = result.values()
+for value in values:
+    items = value["items"]
+    for item in items:
+        date = item["date"]
+        print(date)
+
+
