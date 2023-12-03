d488d5c1b87514858e70b87516e97d0009c8c1f5
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-12-03 19:41:39 +0200

    Upgrade 7-days statistics

--- main.py
+++ main.py
@@ -3,26 +3,10 @@ import os
 from dotenv import load_dotenv
 import datetime
 from pprint import pprint
+import plotly.express as px
+import pandas
 
 load_dotenv()
-
-token = os.environ["TOKEN"]
-params = {
-        "q": "Кока-Кола",
-        "access_token": token,
-        "start_time": 1700949600,
-        "end_time": 1701381600,
-        "v": "5.199"
-    }
-response = requests.get("https://api.vk.com/method/newsfeed.search", params=params)
-response.raise_for_status()
-result = response.json()
-# pprint(result)
-values = result.values()
-for value in values:
-    items = value["items"]
-    for item in items:
-        date = item["date"]
 
 today = datetime.date.today()
 yesterday = today - datetime.timedelta(days=1)
@@ -33,17 +17,40 @@ start_date_time = datetime.datetime(start_date.year, start_date.month, start_dat
 end_date_time = start_date_time - datetime.timedelta(days=1)
 date_tuple = tuple((start_date, start_date_time, end_date_time))
 date_list = []
+token = os.environ["TOKEN"]
+
 for count in range(7):
-    date_tuple = tuple((start_date, end_date_time.timestamp(), start_date_time.timestamp()))
-    date_list.append(date_tuple)
-    start_date = start_date - datetime.timedelta(days=1)
     start_date_time = start_date_time - datetime.timedelta(days=1)
     end_date_time = start_date_time - datetime.timedelta(days=1)
-
-
+    params = {
+        "q": "Кока-Кола",
+        "access_token": token,
+        "start_time": end_date_time.timestamp(),
+        "end_time": start_date_time.timestamp(),
+        "v": "5.199"
+    }
+    response = requests.get("https://api.vk.com/method/newsfeed.search", params=params)
+    response.raise_for_status()
+    result = response.json()
+    values = result.values()
+    for value in values:
+        item_count = value["count"]
+    date_tuple = tuple((start_date, item_count))
+    date_list.append(date_tuple)
+    start_date = start_date - datetime.timedelta(days=1)
 pprint(date_list)
 
 
 
+
+
+
+# data = {
+#     'День': date_list,
+#     'Количество': result["count"]
+# }
+#
+# fig = px.bar(data, x='День', y='Количество', title='График упоминаний Кока-Кола ВКонтакте')
+# fig.show()
 
 
